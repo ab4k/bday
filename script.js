39d0c81f71771ce9fb551c1c98b6fba34df0ef0c
Abhay Kumar <69470032+ab4k@users.noreply.github.com>
2025-12-03 15:39:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -76,7 +76,7 @@ document.getElementById("calendarBtn").addEventListener("click", function () {
     
     // Set your event time here (Google Calendar needs UTC format without punctuation)
     const start = "20251207T000000"; 
-    const end   = "20251207T240000";
+    const end   = "20251207T239999";
 
     const url =
       "https://www.google.com/calendar/render?action=TEMPLATE" +
@@ -86,6 +86,7 @@ document.getElementById("calendarBtn").addEventListener("click", function () {
 
     window.open(url, "_blank");
 });
+
 
 
 
